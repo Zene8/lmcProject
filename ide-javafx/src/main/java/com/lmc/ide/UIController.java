92966013cb95d5a8bc5aa127a10af03a3303b057
Zene8 <nathanj91905@gmail.com>
2025-08-31 07:28:41 +0000

    fix

--- ide-javafx/src/main/java/com/lmc/ide/UIController.java
+++ ide-javafx/src/main/java/com/lmc/ide/UIController.java
@@ -13,6 +13,7 @@ import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
